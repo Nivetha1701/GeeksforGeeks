d6973205c424544a27c03c55dfd0c5ae3d279be6
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-08-27 21:26:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Multiply Array/multiply-array.java
@@ -0,0 +1,11 @@
+
+class Solution {
+
+    public static int product(int arr[], int n) {
+        int prod=1;
+        for(int i=0;i<n;i++){
+            prod*=arr[i];
+        }
+        return prod;
+    }
+}
