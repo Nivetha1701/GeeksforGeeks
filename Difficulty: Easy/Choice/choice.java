ba7a588c8b648b94072e820fb7278e6e9092af4d
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-08-29 21:31:17 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Choice/choice.java
@@ -0,0 +1,16 @@
+// User function Template for Java
+
+class Solution {
+
+    int[] arrayForm(int a, int b, int c) {
+        // code here
+        return new int[]{a,b,c};
+    }
+
+    String stringForm(int a, int b, int c) {
+        
+        // code here
+        String str=String.valueOf(a)+String.valueOf(b)+String.valueOf(c);
+        return str;
+    }
+}
