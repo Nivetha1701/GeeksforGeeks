1149354421fbd38a80c93674dd9209ff87d8ee74
Nivetha1701 <125266596+Nivetha1701@users.noreply.github.com>
2025-09-05 22:54:19 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -0,0 +1,19 @@
+// User function Template for Java
+
+class Solution {
+    static int evenlyDivides(int n) {
+        // code here
+         int original = n;
+        int count = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            n /= 10;
+
+            if (digit != 0 && original % digit == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
